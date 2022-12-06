baf7b52fef2d199d8afcc19d134f7e23048ccbb0
Danilo Araujo <x45danilo45x@gmail.com>
2022-12-06 19:52:28 -0300

    fix modules.

--- sistemaEmissaoCertificado/src/main/java/module-info.java
+++ sistemaEmissaoCertificado/src/main/java/module-info.java
@@ -2,12 +2,20 @@
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens org.example.application.controller to javafx.fxml;
+    //opens org.example.application.controller to javafx.fxml;  ---nao sei se precisa pq tem na linha 13
     opens org.example.application.view to javafx.fxml;
+
      requires itextpdf;
      requires java.desktop;
      requires opencsv;
      requires mail;
-     exports org.example.application.view;
+     requires java.sql;
+  requires sqlite.jdbc;
+  exports org.example.application.view;
      exports org.example.application.controller;
+     exports org.example.domain.entities.administrador;
+     exports org.example.domain.entities.certificado;
+     exports org.example.domain.entities.evento;
+     exports org.example.domain.entities.participante;
+     opens org.example.application.controller to javafx.fxml;
  }
