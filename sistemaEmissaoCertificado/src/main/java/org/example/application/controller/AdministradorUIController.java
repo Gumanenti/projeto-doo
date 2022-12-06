acd7e958fb31bf259d55b7d54c334a8c2cc51f0f
Danilo Araujo <x45danilo45x@gmail.com>
2022-12-06 19:44:46 -0300

    fix bugs controllers administrator.

--- sistemaEmissaoCertificado/src/main/java/org/example/application/controller/AdministradorUIController.java
+++ sistemaEmissaoCertificado/src/main/java/org/example/application/controller/AdministradorUIController.java
@@ -1,11 +1,84 @@
 package org.example.application.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.example.application.view.WindowLoader;
+import org.example.domain.entities.administrador.Administrador;
+
+import java.io.IOException;
+
+import static org.example.application.main.Main.*;
+
 
 public class AdministradorUIController {
-    public void saveOrUpdate(ActionEvent actionEvent) {
+
+
+    @FXML
+    private TextField txtLogin;
+    @FXML
+    private TextField txtSenha;
+    @FXML
+    private TextField txtPalavraChave;
+    @FXML
+    private Button btnConfirm;
+    @FXML
+    private Button btnCancel;
+
+    private Administrador administrador;
+
+    private void getEntityToView(){
+        if(administrador == null){
+            administrador = new Administrador();
+        }
+        administrador.setLogin(txtLogin.getText());
+        administrador.setSenha(txtSenha.getText());
+        administrador.setPalavraChave(txtPalavraChave.getText());
     }
 
-    public void backToPreviewScenne(ActionEvent actionEvent) {
+    private void setEntityIntoView(){
+        txtLogin.setText(administrador.getLogin());
+        txtLogin.setDisable(true);
+        txtSenha.setText(administrador.getSenha());
+        txtPalavraChave.setText(administrador.getPalavraChave());
     }
+
+    public void saveOrUpdate(ActionEvent actionEvent) throws IOException {
+        getEntityToView();
+        if (findAdministradorUseCase.findOne(administrador.getLogin()).isEmpty()){
+            createAdministradorUseCase.insert(administrador);
+        } else {
+            updateAdministradorUseCase.update(administrador);
+        }
+        WindowLoader.setRoot("AdministradorManagementUI");
+    }
+
+    public void backToPreviewScenne(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("AdministradorManagementUI");
+    }
+
+    public void setAdministrador(Administrador administrador, UIMode mode) {
+        if (administrador == null)
+            throw new IllegalArgumentException("Administrador não pode ser nulo");
+
+        this.administrador = administrador;
+        setEntityIntoView();
+
+        if (mode == UIMode.VIEW)
+            configureViewMode();
+
+    }
+
+    private void configureViewMode() {
+        btnCancel.setLayoutX(btnConfirm.getLayoutX());
+        btnCancel.setLayoutX(btnConfirm.getLayoutY());
+        btnCancel.setText("Fechar");
+
+        btnConfirm.setVisible(false);
+        txtLogin.setDisable(true);
+        txtSenha.setDisable(true);
+        txtPalavraChave.setDisable(true);
+    }
+
 }
