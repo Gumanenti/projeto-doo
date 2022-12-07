c392b9c7e8bf13fd20830d3975e6f7e46753fd39
Danilo Araujo <x45danilo45x@gmail.com>
2022-12-07 19:30:19 -0300

    add EnviarCertificadoUI

---
+++ sistemaEmissaoCertificado/src/main/java/org/example/application/controller/EnviarCertificadoUI.java
@@ -0,0 +1,18 @@
+package org.example.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class EnviarCertificadoUI {
+    public void backToPrewview() {
+    }
+
+    public void removeCertificado() {
+    }
+
+    public void sendOneCertificado() {
+    }
+
+    public void sendAllCertificado() {
+
+    }
+}
